a14c83078feaafc6f58a0644e4365b425b6e46e9
matheus2m <matheusrobaskievicz@hotmail.com>
2020-06-10 23:14:41 -0300

    criado metodo para consultar usuario e estilizado lista

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,74 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../service/api';
 
-import { Container } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileText,
+} from './styles';
 
 const Main = () => {
-  return <Container />;
+  const [users, setUsers] = React.useState('');
+  const [newUser, setNewUser] = React.useState('');
+
+  const handleAdduser = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={(text) => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={() => handleAdduser()}
+        />
+        <SubmitButton onPress={() => handleAdduser()}>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+      <List
+        data={users}
+        keyExtractor={(user) => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+            <ProfileButton onPress={() => {}}>
+              <ProfileText>Ver perfil</ProfileText>
+            </ProfileButton>
+          </User>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Main;
